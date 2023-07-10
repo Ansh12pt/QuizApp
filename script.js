4e16c5cf5d4f7c9fc298e451301974c42d06a9d3
Ansh12pt <138846200+Ansh12pt@users.noreply.github.com>
2023-07-10 14:14:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,135 @@
+const questions = [
+    {
+        question: "How many days are there in a leap year?",
+        answers:[
+            { text: "355", correct: false},
+            { text: "360", correct: false},
+            { text: "300", correct: false},
+            { text: "366", correct: true},
+        ]
+    },
+
+    {
+        question: " Which is known as the longest river on the earth?",
+        answers:[
+            { text: "Yamuna", correct: false},
+            { text: "Godavari", correct: false},
+            { text: "Nile", correct: true},
+            { text: "Ganga", correct: false},
+        ]
+    },
+
+    {
+        question: " Where is the Gateway of India located?",
+        answers:[
+            { text: "Mumbai", correct: true},
+            { text: "Dehli", correct: false},
+            { text: "Kanpur", correct: false},
+            { text: "Lucknow", correct: false},
+        ]
+    },
+
+    {
+        question: " How many planets are there in the solar system?",
+        answers:[
+            { text: "9", correct: false},
+            { text: "8", correct: true},
+            { text: "6", correct: false},
+            { text: "7", correct: false},
+        ]
+    },
+
+    {
+        question: " Who won the first T20 World Cup?",
+        answers:[
+            { text: "Pakistan", correct: false},
+            { text: "India", correct: true},
+            { text: "England", correct: false},
+            { text: "Australia", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function  resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+startQuiz();
